f4dd05a4b4c34861bc8ec2b501222bb61da81bd2
phankons <stephen.han@gmx.com>
2023-07-11 22:54:19 -0400

    add property sql

--- db/database.js
+++ db/database.js
@@ -174,12 +174,15 @@ const addProperty = function (property) {
           number_of_bathrooms,
           number_of_bedrooms)
        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-       RETURNING *;`, [property.owner_id, property.title, property.description, 
+       RETURNING *;`, [Number(property.owner_id), 
+                      property.title, property.description, 
                       property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city,
                       property.province, property.post_code, property.country,
-                      property.parking_spaces, property.number_of_bathrooms,
-                      property.number_of_bedrooms])
+                      Number(property.parking_spaces), 
+                      Number(property.number_of_bathrooms),
+                      Number(property.number_of_bedrooms)])
   .then((result) => {
+    console.log(result.rows[0]);
     return result.rows[0];
   })
   .catch((err) => {
